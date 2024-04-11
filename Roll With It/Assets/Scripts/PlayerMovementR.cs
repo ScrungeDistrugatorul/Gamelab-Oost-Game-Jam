835132f91980650eeb4791abede57bba14c126a7
R <517909@student.saxion.nl>
2024-04-11 16:44:37 +0200

    Add player movement

---
+++ Roll With It/Assets/Scripts/PlayerMovementR.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementR : MonoBehaviour
+{
+    //Movement elements:
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpStrength;
+    private float HorizontalSpeed;
+    private float VerticalSpeed;
+    private Vector3 move;
+    
+    private Rigidbody rb;
+    private Vector3 floorNormal;
+
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float airDrag;
+    [SerializeField] private LayerMask IsGround;
+    [SerializeField] private float GroundCheckRad;
+
+    [SerializeField] private Camera camera;
+    [SerializeField] private Vector3 camOffset;
+
+    private void Awake() {
+    }
+
+    private void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {      
+        HorizontalSpeed = Input.GetAxisRaw("Horizontal");
+        VerticalSpeed = Input.GetAxisRaw("Vertical");
+        move = new Vector3(HorizontalSpeed, 0, VerticalSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && OnGround())
+        {
+            Vector3 forward = Vector3.Cross(camera.transform.right, floorNormal);
+            rb.AddForce(floorNormal * jumpStrength);
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            DebugReset();
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            DebugScale(0.2f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            DebugScale(-0.2f);
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            
+        }
+
+        camera.transform.position = transform.position + camOffset;
+
+        Debug.Log($"Velocity {rb.velocity.magnitude}");
+    }
+
+    private void FixedUpdate()
+    {
+        // rb.AddForce(move.normalized * speed, ForceMode.Acceleration);
+        rb.drag = OnGround() ? groundDrag : airDrag;
+        SMBMove(camera.transform.right);
+    }
+
+    private void SMBMove(Vector3 pRight)
+    //TODO: Make player move faster down slope
+    {
+        if (!OnGround()) return;
+        
+        CalculateFloorNormal();
+
+        // if (move.magnitude == 0 && rb.velocity.magnitude > 0)
+        // {
+        //     // rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, speed * -1.2f * Time.deltaTime);
+        // }
+        // else
+        if (move.magnitude != 0)
+        {
+            Vector3 forward = Vector3.Cross(pRight, floorNormal);
+
+            Vector3 forwardForce = speed * move.z * forward;
+            Vector3 rightForce = speed * move.x * pRight;
+            Vector3 forceVec = forwardForce + rightForce;
+            
+            rb.AddForce(forceVec);
+        }
+    }
+
+    private bool OnGround()
+    {
+        return Physics.CheckSphere(transform.position - (Vector3.up * 0.5f), GroundCheckRad, IsGround);
+    }
+
+    private void CalculateFloorNormal()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, IsGround))
+        {
+            floorNormal = hit.normal;
+        }
+    }
+
+    private void DebugReset()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    private void DebugScale(float scaleChange)
+    {
+        transform.localScale += Vector3.one * scaleChange;
+    }
+}
